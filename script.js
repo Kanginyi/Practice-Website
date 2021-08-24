dc5a5408bea0aaf6083601768b6a0188decd64d2
Eric Yi <erickanginyi@gmail.com>
2021-08-24 19:11:51 -0400

    Add click event for brain

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ h2.textContent = "Make sure to watch the video before you go";
 document.querySelector("body").appendChild(h2);
 
 console.log("WHAT AM I SUPPOSED TO ADD HERE WITH JAVASCRIPT");
+
+const never = document.getElementById("never");
+never.addEventListener("click", () => {
+    alert("Never!");
+});
+
+const always = document.getElementById("always");
+always.addEventListener("click", () => {
+    alert("Always!");
+});
