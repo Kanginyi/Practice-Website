98064ea3c47db2985a1d21d4ace021e8fb45281b
Eric Yi <erickanginyi@gmail.com>
2021-08-24 02:16:55 -0400

    Change h2 again

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Make sure to watch the video before you go~";
+h2.textContent = "Make sure to watch the video before you go";
 document.querySelector("body").appendChild(h2);
 
 console.log("WHAT AM I SUPPOSED TO ADD HERE WITH JAVASCRIPT");
